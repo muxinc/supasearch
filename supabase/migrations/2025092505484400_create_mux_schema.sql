4cd47868098f0409e9380284bf69c679938a257e
Dylan Jhaveri <djhaveri@mux.com>
2025-10-16 11:43:21 -0700

    remove the old mux integration

--- supabase/migrations/2025092505484400_create_mux_schema.sql
+++
@@ -1,4 +0,0 @@
--- Create the mux schema for all Mux-related tables and functions
--- This schema will contain assets, live_streams, uploads, and other Mux entities
-
-CREATE SCHEMA IF NOT EXISTS mux;
